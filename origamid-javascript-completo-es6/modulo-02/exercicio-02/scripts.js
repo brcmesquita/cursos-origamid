3cd76a430e11427cc347cd76719c484caa224835
Bruno Raphael Cabral de Mesquita <brcmesquita@gmail.com>
2022-06-24 18:31:45 -0300

    exercício 02 resolvido

---
+++ origamid-javascript-completo-es6/modulo-02/exercicio-02/scripts.js
@@ -0,0 +1,23 @@
+// Declare uma variável contendo uma string
+var banco = 'Caixa Econômica Federal';
+
+// Declare uma variável contendo um número dentro de uma string
+var variNumber = '1';
+
+// Declare uma variável com a sua idade
+var idade = 37;
+
+// Declare duas variáveis,
+// uma com seu nome
+// e outra com seu sobrenome e some as mesmas
+var nome = 'Bruno',
+    sobrenome = 'Raphael';
+
+console.log(nome + ' ' + sobrenome);
+
+// Coloque a seguinte frase em uma variável: It's time
+var frase = `It's time`;
+
+// Verifique o tipo da variável que contém seu nome
+var tipoVariavel = typeof nome;
+console.log(tipoVariavel);
