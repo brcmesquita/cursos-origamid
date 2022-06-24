4411b16c71388d4713ede9a9f79ea2db8e8e27ff
Bruno Raphael Cabral de Mesquita <brcmesquita@gmail.com>
2022-06-24 19:07:56 -0300

    exercicio 03

---
+++ origamid-javascript-completo-es6/modulo-02/exercicio-03/scripts.js
@@ -0,0 +1,33 @@
+// Qual o resultado da seguinte expressão?
+var total = 10 + 5 * 2 / 2 + 20; // 35
+
+// Crie duas expressões que retornem NaN
+var expressao1 = 'Raphael 37' * 5;
+console.log(expressao1);
+
+var expressao2 = '100kg' - 10;
+console.log(expressao2);
+
+// Somar a string '200' com o número 50 e retornar 250
+console.log(+'200' + 50);
+
+// Incremente o número 5 e retorne o seu valor incrementado
+var numero5 = 5;
+console.log(numero5++);
+console.log(numero5);
+
+// Como dividir o peso por 2?
+// var numero = '80';
+// var unidade = 'kg';
+// var peso = numero + unidade; // '80kg'
+// var pesoPorDois = peso / 2; // NaN (Not a Number)
+
+var numero = +'80' / 2; // 40
+var unidade = 'kg';
+var peso = numero + unidade; // '40kg'
+console.log(peso);
+
+
+
+
+
