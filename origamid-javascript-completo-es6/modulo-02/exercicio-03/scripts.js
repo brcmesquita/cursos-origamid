3221d48ea7237183eb8dd019deee068b0d6169aa
Bruno Raphael Cabral de Mesquita <brcmesquita@gmail.com>
2022-06-25 19:09:06 -0300

    Exercício #5

--- origamid-javascript-completo-es6/modulo-02/exercicio-03/scripts.js
+++ origamid-javascript-completo-es6/modulo-02/exercicio-03/scripts.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 // Qual o resultado da seguinte expressão?
 var total = 10 + 5 * 2 / 2 + 20; // 35
 
@@ -25,4 +26,33 @@ console.log(numero5);
 var numero = +'80' / 2; // 40
 var unidade = 'kg';
 var peso = numero + unidade; // '40kg'
+=======
+// Qual o resultado da seguinte expressão?
+var total = 10 + 5 * 2 / 2 + 20; // 35
+
+// Crie duas expressões que retornem NaN
+var expressao1 = 'Raphael 37' * 5;
+console.log(expressao1);
+
+var expressao2 = '100kg' - 10;
+console.log(expressao2);
+
+// Somar a string '200' com o número 50 e retornar 250
+console.log(+'200' + 50);
+
+// Incremente o número 5 e retorne o seu valor incrementado
+var numero5 = 5;
+console.log(numero5++);
+console.log(numero5);
+
+// Como dividir o peso por 2?
+// var numero = '80';
+// var unidade = 'kg';
+// var peso = numero + unidade; // '80kg'
+// var pesoPorDois = peso / 2; // NaN (Not a Number)
+
+var numero = +'80' / 2; // 40
+var unidade = 'kg';
+var peso = numero + unidade; // '40kg'
+>>>>>>> Stashed changes
 console.log(peso);
--- origamid-javascript-completo-es6/modulo-02/exercicio-04/scripts.js
+++ origamid-javascript-completo-es6/modulo-02/exercicio-04/scripts.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 // Verifique se a sua idade é maior do que a de algum parente
 // Dependendo do resultado coloque no console 'É maior', 'É igual' ou 'É menor'
 let minhaIdade = 37, idadeEsposa = 36;
@@ -46,3 +47,50 @@ if(('Gato' === 'gato') || (5 > 2)) {
 } // Cão
 
 addEventListener('click', function(){console.log(this.event.clientY)})
+=======
+// Verifique se a sua idade é maior do que a de algum parente
+// Dependendo do resultado coloque no console 'É maior', 'É igual' ou 'É menor'
+let minhaIdade = 37, idadeEsposa = 36;
+
+if (minhaIdade > idadeEsposa) {
+  console.log("É maior");
+} else if (minhaIdade === idadeEsposa) {
+  console.log("É igual");
+} else {
+  console.log("É menor");
+} // é maior
+
+// Qual valor é retornado na seguinte expressão?
+var expressao = (5 - 2) && (5 - ' ') && (5 - 2); // 3
+
+// Verifique se as seguintes variáveis são Truthy ou Falsy
+var nome = 'Andre'; // truthy
+var idade = 28; // truthy
+var possuiDoutorado = false; // falsy
+var empregoFuturo; // falsy
+var dinheiroNaConta = 0; // falsy
+
+if (dinheiroNaConta) {
+  console.log("Verdadeiro");
+} else {
+  console.log("Falso");
+}
+
+// Compare o total de habitantes do Brasil com China (valor em milhões)
+var brasil = 207;
+var china = 1340;
+
+// O que irá aparecer no console?
+if(('Gato' === 'gato') && (5 > 2)) {
+  console.log('Verdadeiro');
+} else {
+  console.log('Falso');
+}
+
+// O que irá aparecer no console?
+if(('Gato' === 'gato') || (5 > 2)) {
+  console.log('Gato' && 'Cão');
+} else {
+  console.log('Falso');
+}
+>>>>>>> Stashed changes
