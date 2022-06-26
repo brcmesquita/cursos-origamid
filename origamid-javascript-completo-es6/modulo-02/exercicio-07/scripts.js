8b8403f5f63ff14a0fcf7f3dc7577eceb8fcfe68
Bruno Raphael Cabral de Mesquita <brcmesquita@gmail.com>
2022-06-26 12:37:04 -0300

    Exercício 08

---
+++ origamid-javascript-completo-es6/modulo-02/exercicio-07/scripts.js
@@ -0,0 +1,16 @@
+// nomeie 3 propriedades ou métodos de strings
+var nome = 'Bruno Raphael';
+console.log(nome.length); // Propriedade
+console.log(nome.toUpperCase()); // Método
+console.log(nome.indexOf('a')); // Método
+
+// nomeie 5 propriedades ou métodos de elementos do DOM
+document.getElementById;
+document.getElementsByClassName;
+document.appendChild;
+document.getElementsByName;
+document.getElementsByTagName;
+
+// busque na web um objeto (método) capaz de interagir com o clipboard,
+// clipboard é a parte do seu computador que lida com o CTRL + C e CTRL + V
+
