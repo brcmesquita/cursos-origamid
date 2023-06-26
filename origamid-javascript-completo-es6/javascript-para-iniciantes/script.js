9c62269709aa807a413cb32c728da1b9431e6ec4
Raphael Cabral <brcmesquita@gmail.com>
2023-06-26 02:07:02 -0300

    update folders

---
+++ origamid-javascript-completo-es6/javascript-para-iniciantes/script.js
@@ -0,0 +1,15 @@
+// Declarar uma variável com o seu nome
+let nome = "Raphael";
+
+// Declarar uma variável com a sua idade
+let idade = 38;
+
+// Declarar uma variável com a sua comida
+// favorita e não atribuir valor
+let comidaFavorita;
+
+// Atribuir valor a sua comida favorita
+comidaFavorita = "Bucetinha e Cuzinho";
+
+// Declarar 5 variáveis diferentes sem valores
+let cinco, variaveis, diferentes, e, semValores;
